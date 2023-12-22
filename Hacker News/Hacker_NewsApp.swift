edb99b043d8a5ae4721eaa3a6c17d04b0c5ab9df
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2023-12-22 15:37:06 +0100

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Djordje Arandjelovic on 22.12.23..
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
